7b94d555982ba79a42058cd53a62d9e2391bd071
Raunak Chaudhary <raunakc39@gmail.com>
2024-07-17 21:31:24 +0530

    yes

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ import connectToMongose from "./database/db.js";
 import express from 'express';
 import auth from './routes/auth.js';
 import notes from './routes/notes.js'
-import cors from 'cors';
-
+import cors from'cors';
 connectToMongose();
+const app=express();
+const port =4000;
 
-const app = express();
-const port = 4000;
+// handle the cors eror while deploying
+// app.use(cors({
+//     origin: 'https://inotebook-frontened.vercel.app/'
+//   }));
 
-// Handle CORS error while deploying
-app.use(cors({
-    origin: '*'
-}));
+// route this way is not good way to make routes we make new folder routes
 
-app.use(cors({
-    origin: 'https://inotebook-frontened.vercel.app/'
-}));
-
-// Middleware
+// *middleware
 app.use(express.json());
+app.use(cors())
 
-// Available routes
-app.use('/api/auth', auth);
-app.use('/api/notes', notes);
-app.use('/', (req, res) => {
-    res.json("eNotebook backend Api");
-});
+// * Available routes
+app.use('/',(req,res)=>{
+    res.json("eNotebook backend Api")
+})
+app.use('/api/auth',auth);
+app.use('/api/notes',notes);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
